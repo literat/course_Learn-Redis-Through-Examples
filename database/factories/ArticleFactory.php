3ee0e53b65390c59ad993021fd835651d5150eef
literat <tomas@litera.me>
2017-10-31 22:35:41 +0100

    Episode #3

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Article::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(6, true),
+    ];
+});
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,19 @@ Route::get('videos/{id}/download', function ($id) {
 
     return back();
 });
+
+Route::get('articles/trending', function () {
+    $trending = Redis::zrevrange('trending_articles', 0, 2);
+
+    $trending = App\Article::hydrate(
+        array_map('json_decode', $trending)
+    );
+
+    return $trending;
+});
+
+Route::get('articles/{article}', function (App\Article $article) {
+    Redis::zincrby('trending_articles', 1, $article);
+
+    return $article;
+});
